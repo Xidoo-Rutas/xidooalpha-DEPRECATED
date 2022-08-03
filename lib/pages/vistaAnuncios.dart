c2e75d91d8d056c1c0ae1147db84ebba06eae4c7
kevin <polopolopolopoline@gmail.com>
2022-08-03 00:38:40 -0500

    ultimos
    final

--- lib/pages/vistaAnuncios.dart
+++ lib/pages/vistaAnuncios.dart
@@ -35,38 +35,37 @@ class _MostrarNoticia extends State<MostrarNoticia> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-        toolbarHeight: 50.0,
-        backgroundColor: Color.fromARGB(255, 51, 51, 51),
-        centerTitle: true,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(bottomRight: Radius.circular(30), bottomLeft: Radius.circular(30)),
-          
-        ),
+          toolbarHeight: 50.0,
+          backgroundColor: Color.fromARGB(255, 51, 51, 51),
+          centerTitle: true,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(30),
+                bottomLeft: Radius.circular(30)),
           ),
-        body: 
-        
-        Container(
-          margin: EdgeInsets.only(bottom: 70.0,left: 20, right: 20, top: 10),
-        decoration: BoxDecoration(
+        ),
+        body: Container(
+          margin: EdgeInsets.only(bottom: 70.0, left: 20, right: 20, top: 10),
+          decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(20.0),
             color: Color.fromARGB(255, 44, 44, 44),
           ),
           child: ListView(
             children: [
               Container(
-                
-                margin: EdgeInsets.only(top: 20, left: 20, right: 40),
+                  margin: EdgeInsets.only(top: 20, left: 20, right: 40),
                   child: Container(
-                child: Text(
-                  "Escrito por: " + dependencia.text,
-                  textAlign: TextAlign.left,
-                style: const TextStyle(
-            color: Color.fromARGB(255, 255, 255, 255),
-            fontSize: 30.0,
-            fontStyle: FontStyle.normal,
-            fontWeight: FontWeight.bold,
-                ),),
-              )),
+                    child: Text(
+                      titulo.text,
+                      textAlign: TextAlign.left,
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        fontSize: 30.0,
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )),
               Container(
                 decoration: BoxDecoration(
                     //color: Colors.black12
@@ -74,54 +73,60 @@ class _MostrarNoticia extends State<MostrarNoticia> {
                 margin:
                     EdgeInsets.only(top: 20, bottom: 20, left: 25, right: 25),
                 child: Container(
-                  
-                    child: Text(titulo.text,
+                    child: Text(dependencia.text,
                         textAlign: TextAlign.left,
                         style: TextStyle(
-                            fontSize: 30,
+                            fontSize: 15,
                             color: Colors.yellowAccent.shade700,
                             fontWeight: FontWeight.bold,
                             fontStyle: FontStyle.italic)) //titulo
                     ),
               ),
-             
-              
-
               Container(
                 margin: EdgeInsets.only(top: 20, left: 20, right: 20),
                 child: Text(
                   parrafo1.text,
                   textAlign: TextAlign.left,
-                style: const TextStyle(
-            color: Color.fromARGB(255, 255, 255, 255),
-            fontSize: 15.0,
-            fontStyle: FontStyle.normal
-                ),),
+                  style: const TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 15.0,
+                      fontStyle: FontStyle.normal),
+                ),
               ),
               Container(
                 margin: EdgeInsets.only(top: 20, left: 20, right: 20),
                 child: Text(
                   parrafo2.text,
                   textAlign: TextAlign.left,
-                style: const TextStyle(
-            color: Color.fromARGB(255, 255, 255, 255),
-            fontSize: 15.0,
-            fontStyle: FontStyle.normal
-                ),),
+                  style: const TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 15.0,
+                      fontStyle: FontStyle.normal),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+                child: Text(
+                  parrafo3.text,
+                  textAlign: TextAlign.left,
+                  style: const TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 15.0,
+                      fontStyle: FontStyle.normal),
+                ),
               ),
               Container(
                 margin: EdgeInsets.only(top: 20, left: 20, right: 40),
-                child: const Text(
-                  "Correo electronico del escritor:",
+                child: Text(
+                  "Fecha de la publicacion: " + fecha.text,
                   textAlign: TextAlign.left,
-                style: const TextStyle(
-            color: Color.fromARGB(255, 255, 255, 255),
-            fontSize: 15.0,
-            fontStyle: FontStyle.normal
-                ),),
+                  style: const TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 15.0,
+                      fontStyle: FontStyle.normal),
+                ),
               ),
-              
-               Container(
+              Container(
                 margin: EdgeInsets.only(left: 20, right: 20, top: 30),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -137,7 +142,8 @@ class _MostrarNoticia extends State<MostrarNoticia> {
                 ),
               ),
               Container(
-                margin: EdgeInsets.only(left: 20, right: 20, top: 30, bottom: 30),
+                margin:
+                    EdgeInsets.only(left: 20, right: 20, top: 30, bottom: 30),
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisSize: MainAxisSize.max,
